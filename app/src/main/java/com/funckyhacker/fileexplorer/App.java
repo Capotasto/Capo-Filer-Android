ab17457bd7a4d108f856be4363c160352a4399ff
Capotasto <rocker519804@gmail.com>
2018-03-10 13:04:24 -0800

    Added Drawer

--- app/src/main/java/com/funckyhacker/fileexplorer/App.java
+++ app/src/main/java/com/funckyhacker/fileexplorer/App.java
@@ -1,11 +1,12 @@
 package com.funckyhacker.fileexplorer;
 
+import com.funckyhacker.fileexplorer.di.DaggerAppComponent;
 import dagger.android.AndroidInjector;
 import dagger.android.support.DaggerApplication;
 
 public class App extends DaggerApplication {
 
   @Override protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
-    return null;
+    return DaggerAppComponent.builder().create(this);
   }
 }
