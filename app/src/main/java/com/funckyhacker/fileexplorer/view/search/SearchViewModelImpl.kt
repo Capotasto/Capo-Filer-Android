b4a7c3e6d805eb98296b7d578aee623a5880937d
NorioEgi <norio.egi@gmail.com>
2018-06-11 16:59:55 -0700

    Convert java to Kotlin for adapter and search page (#35)

---
+++ app/src/main/java/com/funckyhacker/fileexplorer/view/search/SearchViewModelImpl.kt
@@ -0,0 +1,52 @@
+package com.funckyhacker.fileexplorer.view.search
+
+import android.os.Environment
+import android.text.TextUtils
+import com.funckyhacker.fileexplorer.view.adapter.MainLinearAdapter
+import rx.Emitter
+import rx.Observable
+import java.io.File
+import java.util.*
+import javax.inject.Inject
+
+class SearchViewModelImpl @Inject
+constructor() : SearchViewModel() {
+
+    override lateinit var linearAdapter: MainLinearAdapter
+        private set
+
+    override fun search(query: String): Observable<List<File>> {
+        return Observable.create({ e ->
+            val result = ArrayList<File>()
+
+            val rootDir = File(Environment.getExternalStorageDirectory().path)
+            if (!rootDir.isDirectory || rootDir.listFiles() == null || rootDir.listFiles().isEmpty()) {
+                e.onError(Throwable("Something wrong your External Storage"))
+            }
+            searchDir(result, rootDir, query)
+            e.onNext(result)
+        }, Emitter.BackpressureMode.DROP)
+    }
+
+    override fun init() {
+        linearAdapter = MainLinearAdapter()
+    }
+
+    override fun setData(files: List<File>) {
+        linearAdapter.setData(files)
+    }
+
+    private fun searchDir(result: MutableList<File>, root: File, query: String) {
+        for (file in root.listFiles()) {
+            if (TextUtils.isEmpty(file.name)) {
+                continue
+            }
+            if (file.name.contains(query) || file.name.toLowerCase().contains(query)) {
+                result.add(file)
+            }
+            if (file.isDirectory) {
+                searchDir(result, file, query)//Notice: Recursive Call
+            }
+        }
+    }
+}
